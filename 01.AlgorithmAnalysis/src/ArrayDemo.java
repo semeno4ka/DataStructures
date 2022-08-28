7022472b94c92e20366cdaee5325f59ead6aa714
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-28 15:31:09 -0400

    Array idea

---
+++ 01.AlgorithmAnalysis/src/ArrayDemo.java
@@ -0,0 +1,7 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] scoreArray = new int[10];
+        System.out.println(scoreArray); // [I@35bbe5e8 - starting address (array at 0), each int 4 bytes=> 40 bytes total
+    }
+}
